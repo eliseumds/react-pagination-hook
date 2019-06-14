b5f206b5e046138ca32eecd590a0d95586a24b73
Eliseu dos Santos <eliseumds@gmail.com>
2019-06-14 23:34:07 +1000

    feat(): create the hook with tests

--- src/react-pagination-hook.ts
+++ src/react-pagination-hook.ts
@@ -1,6 +1,126 @@
-// Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
-  // import "core-js/fn/array.find"
-  // ...
-export default class DummyClass {
+import { useCallback, useState } from 'react';
 
+export type ConfigArg = {
+  numberOfPages: number;
+  initialPage: number;
+  maxButtons?: number;
+  showEllipsis?: boolean;
+  padding?: number;
+};
+
+type Config = ConfigArg & {
+  maxButtons: number;
+  padding: number;
+};
+
+export type PaginatorPiece =
+  | {
+      type: 'previous' | 'next';
+      pageNumber: number;
+      isDisabled: boolean;
+    }
+  | {
+      pageNumber: number;
+      type: 'page-number';
+    }
+  | {
+      type: 'ellipsis';
+    };
+
+export interface State {
+  activePage: number;
+  isFirst: boolean;
+  isLast: boolean;
+  hasPrevious: boolean;
+  hasNext: boolean;
+  visiblePieces: PaginatorPiece[];
+  goToPage: (pageNumber: number) => void;
+}
+
+function computeVisiblePieces(activePage: number, config: Config): PaginatorPiece[] {
+  const { numberOfPages, maxButtons, showEllipsis, padding } = config;
+  const visiblePieces: PaginatorPiece[] = [];
+
+  if (numberOfPages <= maxButtons) {
+    for (let page = 1; page <= numberOfPages; page++) {
+      visiblePieces.push({ type: 'page-number', pageNumber: page });
+    }
+
+    return visiblePieces;
+  }
+
+  let lowerLimit = activePage;
+  let upperLimit = activePage;
+
+  visiblePieces.push({
+    type: 'previous',
+    pageNumber: Math.max(1, activePage - 1),
+    isDisabled: activePage === 1,
+  });
+
+  if (activePage > 2) {
+    visiblePieces.push({ type: 'page-number', pageNumber: 1 });
+    visiblePieces.push({ type: 'ellipsis' });
+  }
+
+  // From https://gist.github.com/keon/5380f81393ad98ec19e6
+  for (let i = 1; i < maxButtons && i < numberOfPages; ) {
+    if (lowerLimit > 1) {
+      lowerLimit--;
+      i++;
+    }
+
+    if (i < maxButtons && upperLimit < numberOfPages) {
+      upperLimit++;
+      i++;
+    }
+  }
+
+  for (let i = lowerLimit; i <= upperLimit; i++) {
+    visiblePieces.push({ type: 'page-number', pageNumber: i });
+  }
+
+  if (activePage < numberOfPages - 2) {
+    visiblePieces.push({ type: 'ellipsis' });
+    visiblePieces.push({ type: 'page-number', pageNumber: numberOfPages });
+  }
+
+  visiblePieces.push({
+    type: 'next',
+    pageNumber: Math.min(numberOfPages, activePage + 1),
+    isDisabled: activePage === numberOfPages,
+  });
+
+  return visiblePieces;
+}
+
+export function usePagination(_config: ConfigArg) {
+  if (typeof _config !== 'object') {
+    // tslint:disable-line strict-type-predicates
+    throw new TypeError(
+      `usePagination(config): config must be an object. Go ${typeof _config} instead`
+    );
+  }
+
+  const config: Config = { maxButtons: 5, padding: 2, ..._config };
+  const [activePage, setActivePage] = useState(config.initialPage);
+  const { numberOfPages } = config;
+  const isFirst = activePage === 1;
+  const isLast = activePage === numberOfPages;
+  const hasPrevious = numberOfPages > 1 && activePage > 1;
+  const hasNext = activePage < numberOfPages;
+  const visiblePieces = computeVisiblePieces(activePage, config);
+  const goToPage = useCallback<State['goToPage']>(pageNumber => {
+    setActivePage(pageNumber);
+  }, []);
+
+  return {
+    activePage,
+    isFirst,
+    isLast,
+    hasPrevious,
+    hasNext,
+    visiblePieces,
+    goToPage,
+  };
 }
--- test/react-pagination-hook.test.ts
+++ test/react-pagination-hook.test.ts
@@ -1,14 +1,129 @@
-import DummyClass from "../src/react-pagination-hook"
+import { act, renderHook } from 'react-hooks-testing-library';
+import { usePagination, PaginatorPiece } from '../src/react-pagination-hook';
 
-/**
- * Dummy test
- */
-describe("Dummy test", () => {
-  it("works if true is truthy", () => {
-    expect(true).toBeTruthy()
-  })
+// "!previous:1 1 2 3 ... 100 next:4" >> [
+//   { type: 'previous', pageNumber: 1, isDisabled: true },
+//   { type: 'page-number', pageNumber: 1 },
+//   { type: 'page-number', pageNumber: 2 },
+//   { type: 'page-number', pageNumber: 3 },
+//   { type: 'ellipsis' },
+//   { type: 'page-number', pageNumber: 100 },
+//   { type: 'next', pageNumber: 4, isDisabled: false },
+// ]
+function buildExpectation(fullExpression: string): PaginatorPiece[] {
+  return fullExpression
+    .trim()
+    .split(' ')
+    .map(expression => {
+      if (/^\d+$/.test(expression)) {
+        return { type: 'page-number', pageNumber: Number(expression) };
+      }
 
-  it("DummyClass is instantiable", () => {
-    expect(new DummyClass()).toBeInstanceOf(DummyClass)
-  })
-})
+      if (expression === '...') {
+        return { type: 'ellipsis' };
+      }
+
+      const matches = expression.match(/^([\!]*(previous|next))\:(\d+)$/);
+
+      if (matches) {
+        const bits = matches[0].split(':');
+        let type: 'previous' | 'next';
+        let pageNumber: number = Number(bits[1]);
+        let isDisabled: boolean = false;
+
+        if (bits[0].startsWith('!')) {
+          isDisabled = true;
+          type = bits[0].slice(1) as any;
+        } else {
+          type = bits[0] as any;
+        }
+
+        return { type, pageNumber, isDisabled };
+      }
+
+      throw new TypeError(`buildExpectation(arg): invalid expression "${expression}"`);
+    });
+}
+
+describe('react-pagination-hook', () => {
+  test('is defined', () => {
+    expect(typeof usePagination).toBe('function');
+  });
+
+  test('break without required properties', () => {
+    const hook1 = renderHook(() => usePagination(undefined as any));
+    expect(hook1.result.error).toBeInstanceOf(TypeError);
+
+    const hook2 = renderHook(() => usePagination(1 as any));
+    expect(hook2.result.error).toBeInstanceOf(TypeError);
+  });
+
+  describe('with basic config', () => {
+    const hook = renderHook(() =>
+      usePagination({
+        numberOfPages: 10,
+        maxButtons: 3,
+        initialPage: 1,
+      })
+    );
+
+    test('return initial state on initial render', () => {
+      expect(hook.result.current.activePage).toBe(1);
+      expect(hook.result.current.isFirst).toBe(true);
+      expect(hook.result.current.isLast).toBe(false);
+      expect(hook.result.current.hasPrevious).toBe(false);
+      expect(hook.result.current.hasNext).toBe(true);
+      expect(typeof hook.result.current.goToPage).toBe('function');
+
+      const expectation = '!previous:1 1 2 3 ... 10 next:2';
+
+      expect(hook.result.current.visiblePieces).toStrictEqual(buildExpectation(expectation));
+    });
+
+    test('navigate to the second page', () => {
+      act(() => {
+        hook.result.current.goToPage(2);
+      });
+
+      expect(hook.result.current.activePage).toBe(2);
+      expect(hook.result.current.isFirst).toBe(false);
+      expect(hook.result.current.isLast).toBe(false);
+      expect(hook.result.current.hasPrevious).toBe(true);
+      expect(hook.result.current.hasNext).toBe(true);
+
+      const expectation = 'previous:1 1 2 3 ... 10 next:3';
+
+      expect(hook.result.current.visiblePieces).toStrictEqual(buildExpectation(expectation));
+    });
+
+    test('navigate to the last page', () => {
+      act(() => {
+        hook.result.current.goToPage(10);
+      });
+
+      expect(hook.result.current.activePage).toBe(10);
+      expect(hook.result.current.isFirst).toBe(false);
+      expect(hook.result.current.isLast).toBe(true);
+      expect(hook.result.current.hasPrevious).toBe(true);
+      expect(hook.result.current.hasNext).toBe(false);
+
+      const expectation = 'previous:9 1 ... 8 9 10 !next:10';
+
+      expect(hook.result.current.visiblePieces).toStrictEqual(buildExpectation(expectation));
+    });
+  });
+
+  test('few pages', () => {
+    const hook = renderHook(() =>
+      usePagination({
+        numberOfPages: 5,
+        maxButtons: 10,
+        initialPage: 1,
+      })
+    );
+
+    const expectation = '1 2 3 4 5';
+
+    expect(hook.result.current.visiblePieces).toStrictEqual(buildExpectation(expectation));
+  });
+});
